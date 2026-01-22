1b1fbe041cdfc25cb75ad883c6fb03efdfaf666c
MatheusBarusso <matheusbbarusso@gmail.com>
2026-01-21 23:55:45 -0300

    carousel working + static logos

--- django/core/views.py
+++ django/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from item.models import Category, Item, ItemImage
 
 def index(request):
-    return render(request, 'core/index.html')
+    random_items = Item.objects.filter(images__isnull=False).distinct().order_by("?")[0:6]
+    
+    return render(request, 'core/index.html', {
+        'random_items': random_items,
+    })
 
 def history(request):
     return render(request, 'core/history.html')
--- django/item/views.py
+++ django/item/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Item, ItemImage
+from .models import Category, Item, ItemImage
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category).exclude(pk=pk)[0:3]
 
     return render(request, 'item/detail.html', {
-        'item': item
+        'item': item,
+        'related_items': related_items
     })
+
+def index(request):
+    random_items = Item.objects.filter(images__isnull=False).distinct().order_by('?')[:6]
+
+    return render(request, 'core/index.html', {
+        'randon_items':random_items,
+    })
+
